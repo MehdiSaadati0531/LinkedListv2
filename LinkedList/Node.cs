c5fce26d9fec3b817ca36f34b3a33d19d3b0208d
MahdiSaadatiOrg <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-13 16:44:16 +0430

    Add files via upload

---
+++ LinkedList/Node.cs
@@ -0,0 +1,75 @@
+﻿namespace LinkedList
+{
+    public class Node
+    {
+        public object Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class NodeOperation
+    {
+        Node head = null;
+
+        private bool IsNull() 
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void InsertFirst(object val)
+        {
+            Node newNode = new Node();
+            if (IsNull())
+            {
+                head.Value = val;
+                head.Next = null;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                head.Value = val;
+                head.Next = temp;
+                head = newNode;
+            }
+        }
+
+        public void InsertLast(object val) 
+        {
+            Node newNode = new Node();
+            if (IsNull())
+            {
+                head.Value = val;
+                head.Next = null;
+                head = newNode;
+            }
+            else
+            {
+                Node findLast = head;
+                while (findLast != null) 
+                {
+                    findLast = findLast.Next;
+                }
+                findLast.Next = newNode;
+                newNode.Value = val;
+                newNode.Next = null;
+            }
+        }
+
+        public void Print() 
+        {
+            while (!IsNull())
+            {
+                Node temp = head;
+                Console.WriteLine(temp.Value);
+                temp = temp.Next;
+            }
+        }
+    }
+}
